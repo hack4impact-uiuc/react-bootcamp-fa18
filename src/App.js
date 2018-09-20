67d02a32d0a1ce20a43de586f71104a8e556d58e
Roy Chiu <rychiu13@gmail.com>
2018-09-19 21:12:39 -0500

    Boilerplate code for before introducing states

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return(
-      <h1>Hello World!</h1>
+    return (
+      <div>
+        <h1>Todo List</h1>
+        <ol>
+          // Add the following elements to the todolist:
+          // Walk to latea, See Megha at latea, Get boba, 5x Get more boba
+        </ol>
+      </div>
     );
   }
 }
 
-export default App
+export default App;
